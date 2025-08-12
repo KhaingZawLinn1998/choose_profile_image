31c87d2c2cba15556d25146f8e9a12e301f7587e
MIT\kz.linn <kz.linn@mit.com.mm>
2025-08-12 14:35:49 +0630

    first commit

---
+++ test_example/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:choose_profile_image/choose_profile_image.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Choose Profile Image Example'),
+      ),
+      body: ChooseProfileImage(),
+    );
+  }
+}
